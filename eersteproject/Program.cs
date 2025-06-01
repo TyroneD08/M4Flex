25193ba07e4b33d3f04274266ba9dfd3651698cc
Tyrone <112290278+TyroneD08@users.noreply.github.com>
2025-06-02 01:19:32 +0200

    tot3af?

---
+++ eersteproject/Program.cs
@@ -0,0 +1,36 @@
+﻿
+
+
+// ik kan niet vinden waar build staat, ik zocht het op maar het blijft zeggen in de bar bovenin. ik zie niks
+// alles werkt voor de rest behalve het build gedeelte
+
+
+
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+using System;
+
+namespace Oefening
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int leeftijd = 16;
+            string characterNaam = "Mario";
+            bool isAfgerond = false;
+            float prijs = 19.99f;
+
+            Console.WriteLine("Leeftijd: " + leeftijd);
+            Console.WriteLine("Character Naam: " + characterNaam);
+            Console.WriteLine("Is afgerond: " + isAfgerond);
+            Console.WriteLine("Prijs: " + prijs);
+
+            Console.ReadKey();
+        }
+    }
+}
+
