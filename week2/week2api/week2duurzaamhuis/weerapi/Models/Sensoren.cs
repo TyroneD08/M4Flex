1885529f12ec88c4c888475299ec3c7eb35fd998
Tyrone <112290278+TyroneD08@users.noreply.github.com>
2025-06-26 01:29:30 +0200

    3 bijna

--- week2/week2api/week2duurzaamhuis/weerapi/Models/Sensoren.cs
+++ week2/week2api/week2duurzaamhuis/weerapi/Models/Sensoren.cs
@@ -1,1 +1,9 @@
-x
+namespace newssite.Models;
+
+
+public class NewsItem
+{
+    public string Title { get; set; }
+    public string Content { get; set; }
+    public string ImageUrl { get; set; }
+}
